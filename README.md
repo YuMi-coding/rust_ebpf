d1f9baacd36cf96c127f31b227664150f41bd348
Yu Mi <yxm319@case.edu>
2023-07-25 08:57:13 -0400

    initial update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rust_ebpf
+
 Playground for eBPF with Rust
+
+
+The code is heavily referenced from https://github.com/libbpf/libbpf-rs/tree/master
---
+++ hello-bpfrs/build.rs
@@ -0,0 +1,23 @@
+use libbpf_cargo::SkeletonBuilder;
+use std::env;
+use std::path::PathBuf;
+
+const SRC: &str = "src/bpf/tc.bpf.c";
+
+fn main(){
+    let mut out= PathBuf::from(
+        env::var_os("OUT_DIR").expect(
+            "OUT_DIR environment variable not set"
+        )
+    );
+
+    out.push("tc.skel.rs");
+
+    SkeletonBuilder::new()
+        .source(SRC)
+        .build_and_generate(&out)
+        .unwrap();
+
+    println!("cargo:rerun-if-changed={SRC}");
+
+}
